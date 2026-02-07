e60bb5f41f5a5bb34af467166042a98bb191ddf8
leannatanner <leanna.tanner2002@gmail.com>
2026-02-06 21:04:00 -0600

    Added message memory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -17,10 +18,12 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
     
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages
     )
 
     if response.usage_metadata is None:
